5e06ca6fd85b4366e7fe920edb84310364c8c0e1
David Adamson <david.adamson2304@gmail.com>
2023-03-25 21:51:40 +0100

    feat: remove boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import logo from '@/assets/logo.svg'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href={logo.src} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
